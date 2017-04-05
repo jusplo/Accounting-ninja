02d1038d2ccee4da6fff04a89717ec151cd069c9
jusplo <ploonka@gmail.com>
2017-04-05 19:54:04 +0200

    update

--- src/main/java/pl/sdacademy/course/AccountingNinja/FinanceController.java
+++ src/main/java/pl/sdacademy/course/AccountingNinja/FinanceController.java
@@ -1,6 +1,13 @@
 package pl.sdacademy.course.AccountingNinja;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,14 +23,39 @@ public class FinanceController {
 
 	@RequestMapping(path = "/test", method = { RequestMethod.POST })
 	public String save(@RequestBody Cost lineToSave) throws IOException {
-		fm.add(lineToSave);
+
+		List<Cost> list = get();
+		list.add(lineToSave);
+
+		FileOutputStream fos = new FileOutputStream("costs1.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(list);
+		oos.close();
 
 		return "saved";
 	}
 
+	@SuppressWarnings("unchecked")
 	@RequestMapping(path = "/test", method = { RequestMethod.GET })
 	public List<Cost> get() throws IOException {
 
-		return fm.getList();
+		List<Cost> result = new ArrayList<>();
+
+		if (new File("costs1.txt").exists()) {
+
+			FileInputStream fis = new FileInputStream("costs1.txt");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+
+			try {
+				result = (List<Cost>) ois.readObject();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			ois.close();
+		}
+
+		return result;
 	}
+
 }
